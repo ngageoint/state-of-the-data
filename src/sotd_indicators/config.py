6798c2b17f89a1dd9305fd256ba5bf242e85d63c
Gregory Brunner <gbrunner@esri.com>
2018-01-12 19:19:56 -0600

    update

--- src/sotd_indicators/config.py
+++ src/sotd_indicators/config.py
@@ -9,28 +9,28 @@ pw = ''
 
 # services
 # Services used as Input Variables
-features_url =              'https://tfdmdevtdw1.esri.com:6443/arcgis/rest/services/WFS/TDSG_WFS/FeatureServer/32'
-grid_url =                  'https://tfdmdevtdw1.esri.com:6443/arcgis/rest/services/WFS/TDSG_WFS/FeatureServer/58'
-comparison_features_url =   'https://tfdmdevtdw1.esri.com:6443/arcgis/rest/services/WFS/TDSG_WFS/FeatureServer/32'
+features_url =              ''
+grid_url =                  ''
+comparison_features_url =   ''
 
 # Services to be Updated
-source_lineage_url =        'https://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/SourceLineage/FeatureServer/0'
-currency_url =              'https://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/TemporalCurrency/FeatureServer/0'
+source_lineage_url =        ''
+currency_url =              ''
 temporal_accuracy = ''
-positional_acc_url =        'https://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/PositionalAccuracy/FeatureServer/0'
+positional_acc_url =        ''
 positional_offset_url = ''
-thematic_url =              'https://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/ThematicAccuracy/FeatureServer/0'
-logical_consistency_url =   'https://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/LogicalConsistency/FeatureServer/0'
-completeness_url =          'https://services1.arcgis.com/g2TonOxuRkIqSOFx/arcgis/rest/services/Completeness/FeatureServer/0'
+thematic_url =              ''
+logical_consistency_url =   ''
+completeness_url =          ''
 
 # Ancillary data
-population =        r'C:\PROJECTS\STATE_OF_THE_DATA\DATA\GPT_2012\gpt_2012'
-grid =              r'C:\PROJECTS\STATE_OF_THE_DATA\DATA\TFDM_PythonAPI.gdb\ThematicAccuracy'
-output_fc =         r'C:\PROJECTS\STATE_OF_THE_DATA\DATA\TFDM_PythonAPI.gdb\ThematicAccuracy_1'
-template_fc =       r'C:\PROJECTS\STATE_OF_THE_DATA\DATA\TDS\TDS_v6.1_LAO_FGCM_sub1\TDS_v6.1_LAO_FGCM_sub1.gdb\TDS\TransportationGroundCrv'
-template_gdb =      r'C:\PROJECTS\STATE_OF_THE_DATA\DATA\TDS\TDS_v6.1_LAO_FGCM_sub1\TDS_v6.1_LAO_FGCM_sub1.gdb'
-attr_check_file =   r'C:\PROJECTS\STATE_OF_THE_DATA\DATA\MEEI_Logical_Consistency_Python_v2_20151119_Greg_Edits.xlsx'
-attr_check_tab =    'HADR'
+population =        ''
+grid =              ''
+output_fc =         ''
+template_fc =       ''
+template_gdb =      ''
+attr_check_file =   ''
+attr_check_tab =    ''
 
 # Data fields
 currency_field =            'zi001_sdv'.upper()
