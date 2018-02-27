b82324e848b932e9c6cc275c60eb7c8fd6877a5d
Gregory Brunner <gbrunner@esri.com>
2018-02-27 13:03:48 -0600

    Refactoring the repo to have the webgis tools in a single folder

--- src/distance_tools/similarity/__init__.py
+++
@@ -1,3 +0,0 @@
-from __future__ import division
-from __future__ import absolute_import
-__version__ = "1.0.0"
--- src/geodataset/io/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-contains operations to read/write geodataframe object
-to and from disk.
-"""
-from __future__ import print_function
-from __future__ import division
-from .fileops import from_featureclass, to_featureclass
-from .sqliteio import to_sqlite
-#from .serviceops import from_layer
